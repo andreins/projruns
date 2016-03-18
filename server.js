5af1423e20b7ceba920df50499733ab9eaca0f85
andrei <andreins@ymail.com>
2016-03-18 05:28:36 +0000

    Finished most functionality for the socket server

--- server.js
+++ server.js
@@ -1,43 +1,65 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var usersConnected = 0;
+var usersConnected = {};
 var currentTours = {};
 
 io.on('connection', function(socket) {
-  l('User connected');
-  io.emit('updateStudentsConnected', ++usersConnected);
-
-  socket.on('disconnect', function() {
-    io.emit('updateStudentsConnected',--usersConnected);
-    l('User disconnected');
+  socket.on('userConnected', function(pin) {
+    l('User with pin ' + pin + ' connected!');
+    if (!currentTours[pin]) {
+      usersConnected[pin] = -1;
+      currentTours[pin] = [];
+    }
+    usersConnected[pin] += 1
+    broadcast(pin, 'uc', usersConnected[pin]);
   });
 
-  socket.on('getAssets', function(msg){
+  socket.on('startTour', function(pin) {
+    l('Tour with pin ' + pin + ' has started');
+  });
+
+  socket.on('setAsset', function(key, assetID) {
+    currentTours[key].push(assetID);
+    broadcast(key, 'ga', assetID);
+  });
+
+  socket.on('sendAnswer', function(key, answer) {
+    broadcast(key, 'ra', answer);
+  });
+
+  socket.on('userDisconnected', function(msg) {
+    l('User with pin ' + msg + ' disconnected!');
+    usersConnected[msg] -= 1;
+    broadcast(msg, 'uc', usersConnected[msg]);
+    if(usersConnected[msg] === -1) {
+      currentTours[msg] = null;
+      usersConnected[msg] = null;
+    }
+  });
+
+  socket.on('disconnect', function() {
+  });
+
+  socket.on('getAssets', function(msg) {
     l('User requested current assent, sent back: ');
     io.emit('currentAsset', currentTours[msg]);
   });
-
-  socket.on('setAsset', function(msg){
-    l('Guide set asset to ' + msg);
-    // currentTou
-    io.emit('currentAsset')
-  })
-
-  socket.on('startTour', function(msg){
-    l('Tour with pin ' + msg + ' has started');
-    currentTours[msg] = 0;
-  });
-
 });
 
-http.listen(process.env.PORT || 8080, function(){
+http.listen(process.env.PORT || 8080, function() {
   l('Listening on port 8080');
-})
+});
 
-app.get('/', function(req, res){
-  res.send('You should not be here!');
-})
+app.get('/', function(req, res) {
+  console.log(currentTours);
+});
+
+function broadcast(channel, key, message) {
+  io.emit(channel, key, message)
+  l('Broadcast - Channel ' + channel + ' - Key: '+ key + ' - Message: ' + message);
+
+}
 
 function l(message) {
   console.log('[INFO]' + message);
