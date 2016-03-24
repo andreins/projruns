727cbb3330b47a5716edcab9f67acfdf3d000547
andrei <andreins@ymail.com>
2016-03-24 20:23:15 +0000

    Latest changes

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var usersConnected = {};
 var currentTours = {};
+var pinz;
 
 io.on('connection', function(socket) {
   socket.on('userConnected', function(pin) {
     l('User with pin ' + pin + ' connected!');
     if (!currentTours[pin]) {
+      pinz = pin;
       usersConnected[pin] = -1;
       currentTours[pin] = [];
     }
@@ -28,6 +30,10 @@ io.on('connection', function(socket) {
     broadcast(key, 'ra', answer);
   });
 
+  socket.on('publishQuestion', function(key, id) { 
+    broadcast(key, 'pq', id);
+  });
+
   socket.on('userDisconnected', function(msg) {
     l('User with pin ' + msg + ' disconnected!');
     usersConnected[msg] -= 1;
@@ -36,6 +42,16 @@ io.on('connection', function(socket) {
       currentTours[msg] = null;
       usersConnected[msg] = null;
     }
+  });
+
+  socket.on('publishAnswer', function(pin) {
+    io.emit(pin, 'pa');
+    l('Publishing answer from tour with pin ' + pin);
+  });
+
+  socket.on('backPressed', function(pin) {
+    l('Publishing answer from tour with pin ' + pin);
+    io.emit(pin, 'back');
   });
 
   socket.on('disconnect', function() {
@@ -52,7 +68,7 @@ http.listen(process.env.PORT || 8080, function() {
 });
 
 app.get('/', function(req, res) {
-  console.log(currentTours);
+  broadcast(pinz, 'ga', 1);
 });
 
 function broadcast(channel, key, message) {
